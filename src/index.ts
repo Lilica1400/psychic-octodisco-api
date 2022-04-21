b4549838c256d439de82cf691c1733860be74def
Gabriel <gabrielcabeca26@gmail.com>
2022-04-20 21:40:49 -0300

    ok

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,13 @@
 import express, { Express, Request, Response } from 'express';
 import 'dotenv/config';
 
-<<<<<<< HEAD
-const PORT = process.env.APP_PORT || 3000;
-=======
 const port = process.env.APP_PORT || 3000;
->>>>>>> 25b6384f08eb6879ecb505e698aa4fc1fae19a2b
 const app: Express = express();
 
 app.use(express.json());
 
 app.get('/', (req: Request, res: Response) => {
-<<<<<<< HEAD
-  res.send('<h1>Hello from the TypeScript world!</h1>');
-});
-
-app.listen(PORT, () => console.log(`Running on ${PORT} ⚡`));
-=======
   res.send(`<h1>Hello from the TypeScript world!</h1>${req.params.id}.`);
 });
 
 app.listen(port, () => console.log(`Running on ${port} ⚡`));
->>>>>>> 25b6384f08eb6879ecb505e698aa4fc1fae19a2b
